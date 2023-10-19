bb3a1dd9dcd8b6a428f5ad0a9940b685ae169893
Nurjon <nurjon2477@gmail.com>
2023-10-19 22:39:36 +0500

    end project

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -4,10 +4,3 @@ export {default as Card} from "./cardd.jpg";
 export {default as Appstore} from "./app.png";
 export {default as Google} from "./google.png";
 export {default as BillingImg} from "./billing.jpg";
-export {default as Star } from "./star.jpg";
-export {default as Telegram} from "./telegram.jpg";
-export {default as Phone} from "./phone.jpg";
-export {default as Namers} from "./namer.jpg"
-export {default as Github} from "./github.jpg";
-export {default as Vero } from "./vero.jpg";
-export {default as Cinema } from "./cinema.jpg"
